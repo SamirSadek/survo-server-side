3906537dbef7b8fdd1333f7880d71e599f6529ff
Samir Sadek <samirsadek2009@gmail.com>
2023-11-29 16:14:01 +0600

    delete survey

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cmjacbf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,9 +32,33 @@ async function run() {
     // await client.connect();
 
     const surveyCollection = client.db("survoDB").collection("surveys")
+    const userCollection = client.db("survoDB").collection("users")
+
+    // users
+    app.get('/users', async(req, res)=>{
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
+    app.post('/users', async(req, res) =>{
+        const user = req.body
+
+        const query = {email: user.email}
+        const existingUser = await userCollection.findOne(query)
+        if(existingUser){
+            return res.send({message : 'user already exists', insertId: null})
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
 
     app.get('/surveys', async(req,res) =>{
         const result = await surveyCollection.find().toArray()
+        res.send(result)
+    })
+    app.delete('/user/:id', async(req, res) =>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await userCollection.deleteOne(query)
         res.send(result)
     })
 
